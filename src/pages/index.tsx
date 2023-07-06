5ff5c9544d00b197a65c974d8ce2891bac1c3f7e
Aadam Ishmael <Aadam@crabclaw.co.uk>
2023-07-06 16:43:06 +0100

    test build

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,7 @@ import { HeroTitle, MetaData, PageWrapper } from "@/components";
 import { interpolateString } from "../utils/interpolateString";
 import { appName } from "../appConfig";
 
-export default function Home({
-  subtitle,
-  authorSummary,
-  currentWorkplace,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Home(props: any) {
   return (
     <>
       <PageWrapper
@@ -20,19 +16,19 @@ export default function Home({
           <Grid item sm={12} md={6}>
             <HeroTitle sx={{ mb: 1 }}>{appName}</HeroTitle>
             <Box sx={{ mb: 5 }}>
-              <Typography variant="h5">{subtitle}</Typography>
-              <Typography variant="h5">
+              {/* <Typography variant="h5">{subtitle}</Typography> */}
+              {/* <Typography variant="h5">
                 Currently working @{" "}
                 <span style={{ fontWeight: "bold" }}>{currentWorkplace}</span>
-              </Typography>
+              </Typography> */}
             </Box>
-            <Typography gutterBottom variant="body2">
+            {/* <Typography gutterBottom variant="body2">
               {interpolateString(
                 authorSummary,
                 "{EXPERIENCE_YEARS}",
                 `${new Date().getFullYear() - 2007}`
               )}
-            </Typography>
+            </Typography> */}
           </Grid>
           <Grid item sm={12} md>
             {/* TODO: create image or animation */}
@@ -52,30 +48,30 @@ export default function Home({
   );
 }
 
-type ContentfulReponse = {
-  subtitle: string;
-  authorSummary: string;
-  currentWorkplace: string;
-};
+// type ContentfulReponse = {
+//   subtitle: string;
+//   authorSummary: string;
+//   currentWorkplace: string;
+// };
 
-export const getServerSideProps: GetServerSideProps<
-  ContentfulReponse
-> = async () => {
-  const { CONTENTFUL_SPACE_ID, CONTENTFUL_PREVIEW_ACCESS_TOKEN } = process.env;
+// export const getServerSideProps: GetServerSideProps<
+//   ContentfulReponse
+// > = async () => {
+//   const { CONTENTFUL_SPACE_ID, CONTENTFUL_PREVIEW_ACCESS_TOKEN } = process.env;
 
-  const baseUrl = "https://preview.contentful.com";
-  const space_id = CONTENTFUL_SPACE_ID;
-  const access_token = CONTENTFUL_PREVIEW_ACCESS_TOKEN;
-  const environment_id = "master";
-  const entry_id = "4hFPXBuB7pYRhaWIvL4CWT";
+//   const baseUrl = "https://preview.contentful.com";
+//   const space_id = CONTENTFUL_SPACE_ID;
+//   const access_token = CONTENTFUL_PREVIEW_ACCESS_TOKEN;
+//   const environment_id = "master";
+//   const entry_id = "4hFPXBuB7pYRhaWIvL4CWT";
 
-  const result = await fetch(
-    `${baseUrl}/spaces/${space_id}/environments/${environment_id}/entries/${entry_id}?access_token=${access_token}`
-  ).then((res) => res.json());
+//   const result = await fetch(
+//     `${baseUrl}/spaces/${space_id}/environments/${environment_id}/entries/${entry_id}?access_token=${access_token}`
+//   ).then((res) => res.json());
 
-  return {
-    props: {
-      ...result.fields,
-    },
-  };
-};
+//   return {
+//     props: {
+//       ...result.fields,
+//     },
+//   };
+// };
